161c1370855e4fec074447254e8ca93628d775ef
GMOkeefe <35286980+GMOkeefe@users.noreply.github.com>
2020-03-07 01:25:13 -0800

    create gui hello world

---
+++ src/com/lab7/gui/controller/HelloWorldController.java
@@ -0,0 +1,19 @@
+package com.lab7.gui.controller;
+
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+import com.lab7.lib.Test;
+
+public class HelloWorldController
+{
+    @FXML
+    public Button hello = null;
+
+    @FXML
+    public void pressButton(Event e)
+    {
+        hello.setText(Test.getTest());
+    }
+}
