9670e0e5b04d47b85d81a587f4d6235b3815560e
GMOkeefe <35286980+GMOkeefe@users.noreply.github.com>
2020-03-08 17:45:02 -0700

    adjusted gui hello world

--- src/com/lab7/gui/controller/HelloWorldController.java
+++ src/com/lab7/gui/controller/HelloWorldController.java
@@ -2,18 +2,18 @@ package com.lab7.gui.controller;
 
 import javafx.event.Event;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
 import com.lab7.lib.Hello;
 
 public class HelloWorldController
 {
     @FXML
-    public Button hello = null;
+    public Label label = null;
 
     @FXML
     public void pressButton(Event e)
     {
-        hello.setText(Hello.getTest());
+        label.setText(Hello.getTest());
     }
 }
