cba5a04e6fd3944560c14241213069a501c7b883
ncpalmie <ncpalmie@calpoly.edu>
2020-03-13 14:39:49 -0700

    Proposed action handler class so console and gui can have separate 'front-end' code and make common calls to handler for common operations

--- src/com/lab7/lib/ActionHandler.java
+++ src/com/lab7/lib/ActionHandler.java
@@ -1,4 +1,56 @@
 package com.lab7.lib;
 
+/**
+ * Action handler for actions created by console/gui user inputs
+ */
 public class ActionHandler {
+    Action currentAction;
+    enum Action {
+        VIEW_ROOMS,
+        CREATE_RES,
+        ALTER_RES,
+        CANCEL_RES,
+        VIEW_ALL_RES,
+        VIEW_REVENUE
+    }
+
+    public ActionHandler() {
+    }
+
+    /**
+     * Takes user input and handles desired action
+     *
+     * @param actionNumber The desired action number entered by the user in the
+     *                     console/associated number from button press in GUI
+     *
+     * @return Formatted string containing desired information from database
+     */
+    public String handleAction(int actionNumber) {
+        String retString = "";
+
+        this.currentAction = Action.values()[actionNumber];
+
+        switch (this.currentAction) {
+            case VIEW_ROOMS:
+                //View room code/functions
+                break;
+            case CREATE_RES:
+                //Reserve room code/functions
+                break;
+            case ALTER_RES:
+                //Alter reservation code/functions
+                break;
+            case CANCEL_RES:
+                //Cancel reservation code/functions
+                break;
+            case VIEW_ALL_RES:
+                //View all reservations code/functions
+                break;
+            case VIEW_REVENUE:
+                //View revenue code/functions
+                break;
+        }
+
+        return retString;
+    }
 }
