e16eb14040d7f290ca2986beb5edee2059c5c553
ncpalmie <ncpalmie@calpoly.edu>
2020-03-13 17:36:10 -0700

    Finished action handler outline, added results so console/gui can have back and forth communication with handler

--- src/com/lab7/lib/ActionHandler.java
+++ src/com/lab7/lib/ActionHandler.java
@@ -1,17 +1,41 @@
 package com.lab7.lib;
 
+import java.math.BigDecimal;
+import java.sql.Date;
+import java.util.List;
+import java.util.ArrayList;
+
 /**
  * Action handler for actions created by console/gui user inputs
  */
 public class ActionHandler {
     Action currentAction;
+    Results actionResult;
+    List<String> prevArgs;
+    boolean continueAction = false;
+
+
     enum Action {
         VIEW_ROOMS,
         CREATE_RES,
         ALTER_RES,
         CANCEL_RES,
-        VIEW_ALL_RES,
+        VIEW_RES,
         VIEW_REVENUE
+    }
+
+    /**
+     * SUCCESS when action is done and return string contains final output
+     *     information for action
+     * PROMPT_AGAIN when action requires more user input to complete and
+     *     return string is a new prompt to return to console/GUI
+     * FAIL when action cannot complete properly and return string contains
+     *     error information for user to correct
+     */
+    public enum Results {
+        SUCCESS,
+        PROMPT_AGAIN,
+        FAIL
     }
 
     public ActionHandler() {
@@ -23,34 +47,108 @@ public class ActionHandler {
      * @param actionNumber The desired action number entered by the user in the
      *                     console/associated number from button press in GUI
      *
-     * @return Formatted string containing desired information from database
+     * @return Formatted string containing desired information from database or
+     * action status for user
      */
-    public String handleAction(int actionNumber) {
-        String retString = "";
+    public String handleAction(int actionNumber, List<String> argsList) {
+        if (actionNumber != -1) {
+            this.currentAction = Action.values()[actionNumber];
+            this.continueAction = false;
+        }
+        else {
+            this.continueAction = true;
+        }
 
-        this.currentAction = Action.values()[actionNumber];
+        //In case action with multiple prompts needs previous information
+        this.prevArgs = argsList;
 
         switch (this.currentAction) {
             case VIEW_ROOMS:
-                //View room code/functions
-                break;
+                return this.getRooms();
             case CREATE_RES:
-                //Reserve room code/functions
-                break;
+                return this.createReservation(argsList);
             case ALTER_RES:
-                //Alter reservation code/functions
-                break;
+                return this.alterReservation(argsList);
             case CANCEL_RES:
-                //Cancel reservation code/functions
-                break;
-            case VIEW_ALL_RES:
-                //View all reservations code/functions
-                break;
+                return this.cancelReservation(argsList);
+            case VIEW_RES:
+                return this.viewReservation(argsList);
             case VIEW_REVENUE:
-                //View revenue code/functions
-                break;
+                return this.viewRevenue();
         }
+        return "No matching action was found.";
+    }
 
+    private String getRooms() {
+        String retString = "";
+
+        //Default successful return
+        this.actionResult = Results.SUCCESS;
         return retString;
     }
+
+    private String createReservation(List<String> argsList) {
+        String retString = "";
+
+        if (this.continueAction) {
+            //For confirmation/other reservation selection
+        }
+        else {
+            //For initial reservation request
+        }
+
+        //Default successful return
+        this.actionResult = Results.SUCCESS;
+        return retString;
+    }
+
+    private String alterReservation(List<String> argsList) {
+        String retString = "";
+
+        if (this.continueAction) {
+            //For confirmation of alteration
+        }
+        else {
+            //For initial alteration request
+        }
+
+        //Default successful return
+        this.actionResult = Results.SUCCESS;
+        return retString;
+    }
+
+    private String cancelReservation(List<String> argsList) {
+        String retString = "temp";
+
+        if (this.continueAction) {
+            //For cancellation confirmation
+        }
+        else {
+            //For initial cancellation request
+        }
+
+        //Default successful return
+        this.actionResult = Results.SUCCESS;
+        return retString;
+    }
+
+    private String viewReservation(List<String> argsList) {
+        String retString = "";
+
+        //Default successful return
+        this.actionResult = Results.SUCCESS;
+        return retString;
+    }
+
+    private String viewRevenue() {
+        String retString = "";
+
+        //Default successful return
+        this.actionResult = Results.SUCCESS;
+        return retString;
+    }
+
+    public Action getCurrentAction()         { return currentAction; }
+    public Results getActionResult()         { return actionResult; }
+    public List<String> getPrevArgs()        { return prevArgs;}
 }
